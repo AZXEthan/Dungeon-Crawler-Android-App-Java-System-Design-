fdade3f341b1a867b84c60db43a5f82f409776f3
AZXEthan <anzexiu@gmail.com>
2023-09-30 20:04:23 -0400

    implemented temporary score feature

--- app/src/main/java/com/example/a2340a_team10/view/ThirdRoom.java
+++ app/src/main/java/com/example/a2340a_team10/view/ThirdRoom.java
@@ -4,17 +4,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.a2340a_team10.R;
+import com.example.a2340a_team10.model.Player;
+import com.example.a2340a_team10.viewmodel.PlayerView;
 
 public class ThirdRoom extends AppCompatActivity {
+    private Player hero;
+    private PlayerView gameViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.third_room);
 
+        hero = Player.getPlayer();
+        gameViewModel = new ViewModelProvider(this).get(PlayerView.class);
+        TextView scoreTextView = findViewById(R.id.scoreTextView);
+
+        // Observe the scoreLiveData to update the score in real-time
+        gameViewModel.getScoreLiveData().observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer score) {
+                // Update the score TextView
+                scoreTextView.setText("Score: " + score);
+            }
+        });
 
         Button goToFirstRoomButton = findViewById(R.id.goToFirstRoomButton);
 
--- app/src/main/java/com/example/a2340a_team10/viewmodel/PlayerView.java
+++ app/src/main/java/com/example/a2340a_team10/viewmodel/PlayerView.java
@@ -1,9 +1,57 @@
 package com.example.a2340a_team10.viewmodel;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
 import com.example.a2340a_team10.model.*;
 
-public class PlayerView {
-    private Player player;
+import java.util.Timer;
+import java.util.TimerTask;
 
+public class PlayerView extends ViewModel {
+    private MutableLiveData<Integer> scoreLiveData = new MutableLiveData<>();
+    private Timer scoreTimer = new Timer();
+    private int scoreDecreaseAmount = 10; // Set the amount to decrease score per second
+    private Player hero = Player.getPlayer();
 
+    public PlayerView() {
+        // Initialize the score LiveData with the starting score
+        scoreLiveData.setValue(hero.getScore());
+
+        // Schedule a task to decrease the score every second
+        scoreTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                decreaseScore();
+            }
+        }, 1000, 1000); // Decrease score every 1000 milliseconds (1 second)
+    }
+
+    // Function to decrease the score
+    private void decreaseScore() {
+        Integer currentScore = scoreLiveData.getValue();
+        if (currentScore != null && currentScore > 0) {
+            int updatedScore = currentScore - scoreDecreaseAmount;
+            scoreLiveData.postValue(updatedScore); // Update the LiveData
+            hero.setScore(updatedScore);
+        }
+    }
+
+    // Expose the LiveData for observation
+     public LiveData<Integer> getScoreLiveData() {
+        return scoreLiveData;
+     }
+
+    // Optional: You can also provide a method to reset the score
+    // public void resetScore(int newScore) {
+    //      scoreLiveData.setValue(newScore);
+    //  }
+
+    // Cleanup the timer when the ViewModel is no longer needed
+//    @Override
+//    protected void onCleared() {
+//        super.onCleared();
+//        scoreTimer.cancel();
+//    }
 }
