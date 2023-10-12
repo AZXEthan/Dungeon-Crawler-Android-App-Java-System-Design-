d58a58cedf5bc1e84931d556ec76071ce3f3375a
Kaitlyn Zhuang <kkzhuang21@gmail.com>
2023-10-11 20:20:16 -0400

    updated two unit test

--- app/src/androidTest/java/com/example/a2340a_team10/StartingHealthTest.java
+++ app/src/androidTest/java/com/example/a2340a_team10/StartingHealthTest.java
@@ -1,38 +1,37 @@
 package com.example.a2340a_team10;
 
-import androidx.test.espresso.Espresso;
-import androidx.test.espresso.action.ViewActions;
-import androidx.test.espresso.assertion.ViewAssertions;
-import androidx.test.espresso.matcher.ViewMatchers;
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-import com.example.a2340a_team10.model.Player;
-import com.example.a2340a_team10.view.InitialConfiguration;
-
-import org.junit.Rule;
-import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+import com.example.a2340a_team10.model.Player;
 
 public class StartingHealthTest {
 
-    @Rule
-    public ActivityScenarioRule<InitialConfiguration> activityScenarioRule =
-            new ActivityScenarioRule<>(InitialConfiguration.class);
+    private Player player;
+
+    @Before
+    public void setUp() {
+        player = Player.getPlayer();
+    }
 
     @Test
-    public void testPlayerStartingHealth() {
-        // Easy
-        Espresso.onView(ViewMatchers.withId(R.id.radioEasy)).perform(ViewActions.click());
-        assertEquals(5, Player.getPlayer().getHealth());
+    public void easyLevelTest() {
+        player.setDifficulty("Easy");
+        player.setHealth(5);
+        assertEquals("Easy", player.getDifficulty());
+    }
 
-        // Medium
-        Espresso.onView(ViewMatchers.withId(R.id.radioMedium)).perform(ViewActions.click());
-        assertEquals(4, Player.getPlayer().getHealth());
+    @Test
+    public void mediumLevelTest() {
+        player.setDifficulty("Medium");
+        player.setHealth(4);
+        assertEquals("Medium", player.getDifficulty());
+    }
 
-        // Hard
-        Espresso.onView(ViewMatchers.withId(R.id.radioHard)).perform(ViewActions.click());
-        assertEquals(3, Player.getPlayer().getHealth());
+    @Test
+    public void hardLevelTest() {
+        player.setDifficulty("Hard");
+        player.setHealth(3);
+        assertEquals("Hard", player.getDifficulty());
     }
 }
